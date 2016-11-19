f7cd2281a063b1fe6b31d9b5472a4562e4907ce9
Matias Cairo <maticairo@gmail.com>
2016-11-19 16:58:30 -0300

    En el cliente tambien hay properties

--- cliente/src/main/java/prograavanzada2016/anotherworld/cliente/App.java
+++ cliente/src/main/java/prograavanzada2016/anotherworld/cliente/App.java
@@ -1,9 +1,14 @@
 package prograavanzada2016.anotherworld.cliente;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import prograavanzada2016.anotherworld.interfaces.VentanaMapa;
 
 public class App {
 	public static void main(String[] args) throws Exception{
-		ClienteJugable cliente = new ClienteJugable("localhost",444);
+		Properties propiedades = new Properties();
+		propiedades.load(new FileInputStream(System.getProperty("user.dir") +"\\src\\main\\resources\\cfg.properties"));
+		ClienteJugable cliente = new ClienteJugable(propiedades.getProperty("ip"), Integer.parseInt(propiedades.getProperty("puerto")));
 	}
 }
