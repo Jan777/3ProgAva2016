01d834cf34b23d003249213c77de090a70d7b9a3
Lukki20 <lukki.07@hotmail.com>
2016-11-20 00:44:24 -0300

    Comienzo de Test de capa Cliente
    Test

---
+++ cliente/src/test/java/cliente/TestLogin.java
@@ -0,0 +1,17 @@
+package cliente;
+
+import org.junit.Assert;
+import org.junit.Test;
+import prograavanzada2016.anotherworld.modelos.Usuario;
+
+public class TestLogin {
+	
+	@Test
+	public void loginUsuario(){
+		
+		Usuario usuario = new Usuario(1, "Lucas", "Nicoloso", "Luki", "123", null);
+		
+		
+	}
+
+}
