9c71113f0f458e9b8c7703e48986933f3c7bdf28
Lukki20 <lukki.07@hotmail.com>
2016-11-23 01:45:38 -0300

    Implementado LOG
    Finalizado

--- cliente/src/main/java/prograavanzada2016/anotherworld/cliente/App.java
+++ cliente/src/main/java/prograavanzada2016/anotherworld/cliente/App.java
@@ -2,18 +2,32 @@ package prograavanzada2016.anotherworld.cliente;
 
 import java.io.FileInputStream;
 import java.util.Properties;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 import prograavanzada2016.anotherworld.interfaces.VentanaMapa;
 
 public class App {
+	
+	private static final Logger LOGGER = Logger.getLogger( "App Cliente");
+	static FileHandler fh;
+	
 	public static void main(String[] args){
 		Properties propiedades = new Properties();
 		try{
+			fh = new FileHandler("C:\\Users\\lukki\\Desktop\\JuegoProgra\\jrpg\\cliente\\src\\main\\java\\prograavanzada2016\\anotherworld\\cliente\\Cliente.log");
+			LOGGER.addHandler(fh);
+	        SimpleFormatter formatter = new SimpleFormatter();  
+	        fh.setFormatter(formatter);
 			propiedades.load(new FileInputStream(System.getProperty("user.dir") +"\\src\\main\\resources\\cfg.properties"));
 			ClienteJugable cliente = new ClienteJugable(propiedades.getProperty("ip"), Integer.parseInt(propiedades.getProperty("puerto")));
+			
 		}
 		catch(Exception e){
 			System.out.print("Ocurrió un error, para más información vea el LOG del sistema.");
+			LOGGER.info("No se pudo iniciar el cliente, verifique archivo de propiedades");
 		}
 		
 	}
--- servidor/src/main/java/prograavanzada2016/anotherworld/servidor/App.java
+++ servidor/src/main/java/prograavanzada2016/anotherworld/servidor/App.java
@@ -1,13 +1,28 @@
 package prograavanzada2016.anotherworld.servidor;
 
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 public class App {
+	
+	private static final Logger LOGGER = Logger.getLogger( "App Servidor");
+	static FileHandler fh;
+	
 	public static void main(String[] args){
 		try{
+			fh = new FileHandler("C:\\Users\\lukki\\Desktop\\JuegoProgra\\jrpg\\servidor\\bin\\prograavanzada2016\\anotherworld\\servidor\\Servidor.log");
+			LOGGER.addHandler(fh);
+			SimpleFormatter formatter = new SimpleFormatter();  
+	        fh.setFormatter(formatter);
 			Servidor server = new Servidor();
 			server.iniciarServidor();
+			
 		}
 		catch(Exception e){
 			System.out.print("Ocurrió un error, para más información vea el LOG del sistema.");
+			LOGGER.info("No se pudo iniciar el servidor, por favor verificar si el puerto se encuentra disponible");
 		}
 		
 	}
