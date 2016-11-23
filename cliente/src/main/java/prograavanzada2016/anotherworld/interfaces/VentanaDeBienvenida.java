35532ac651eab9ceb4367451ec9210616fca46b6
Matias Cairo <maticairo@gmail.com>
2016-11-23 17:34:04 -0300

    Funcionando progressbar

--- cliente/src/main/java/prograavanzada2016/anotherworld/interfaces/VentanaDeBienvenida.java
+++ cliente/src/main/java/prograavanzada2016/anotherworld/interfaces/VentanaDeBienvenida.java
@@ -6,6 +6,7 @@ import prograavanzada2016.anotherworld.mensajes.request.LoginMessage;
 import prograavanzada2016.anotherworld.mensajes.request.PersonajeConsultaMessage;
 import prograavanzada2016.anotherworld.modelos.Usuario;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
@@ -23,18 +24,21 @@ import javax.swing.Timer;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 
 import com.google.gson.Gson;
 
 import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Toolkit;
 import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 import javax.swing.JProgressBar;
 
 public class VentanaDeBienvenida extends JFrame {
@@ -46,6 +50,7 @@ public class VentanaDeBienvenida extends JFrame {
     static Properties propiedades;
     private Timer timer;
     private JProgressBar progressBar;
+    private ActionListener al;
 
 	public VentanaCrearPersonaje ventanaCrearPersonaje;
 	
@@ -53,8 +58,8 @@ public class VentanaDeBienvenida extends JFrame {
 
 	public VentanaDeBienvenida(ClienteJugable clienteJugable) throws Exception {
 		this.clienteJugable=clienteJugable;
-		//Image image = new ImageIcon("src/main/resources/IconoVentana.jpg").getImage();
-		//setIconImage(image);
+		Image image = new ImageIcon("src/main/resources/IconoVentana.jpg").getImage();
+		setIconImage(image);
 		//setIconImage(Toolkit.getDefaultToolkit().getImage(VentanaDeBienvenida.class.getResource("src/main/resources/IconoVentana.jpg")));
 		setTitle("Bienvenido!");
 		initComponents();
@@ -71,14 +76,26 @@ public class VentanaDeBienvenida extends JFrame {
 		lblBienvenido.setText("Bienvenido "+usuario.getNombre());
 		
 		JLabel label = new JLabel("");
-		//Image image2 = new ImageIcon("src/main/resources/VentanaPrincipal.jpg").getImage();
-		//label.setIcon(new ImageIcon(image2));
+		Image image2 = new ImageIcon("src/main/resources/VentanaPrincipal.jpg").getImage();
+		label.setIcon(new ImageIcon(image2));
 		//label.setIcon(new ImageIcon(VentanaDeBienvenida.class.getResource("src/main/resources/VentanaPrincipal.jpg")));
 		label.setBounds(0, 0, 359, 236);
 		contentPane.add(label);
 	}
 	
 	public void initComponents() throws Exception{
+		al= new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(progressBar.getValue() < 100){
+					progressBar.setValue(progressBar.getValue()+10);
+				}else{
+					timer.stop();
+				}
+								
+			}
+		};
+		timer = new Timer(100, al);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(new Dimension(375,275));
 		contentPane = new JPanel();
@@ -97,8 +114,9 @@ public class VentanaDeBienvenida extends JFrame {
 			}
 		});
 		
-		progressBar = new JProgressBar(0, 2000);
+		progressBar = new JProgressBar(0, 110);
 		progressBar.setBounds(27, 156, 293, 31);
+		progressBar.setValue(0);
 		progressBar.setStringPainted(true);
 		progressBar.setForeground(Color.GREEN);
 		contentPane.add(progressBar);
@@ -106,29 +124,20 @@ public class VentanaDeBienvenida extends JFrame {
 		contentPane.add(jugarButton);
 	}
 	
-	public void iterate(){
-		 	int num=0;
-			while (num < 100) {
-		 		progressBar.setValue(num); // Asignar un valor a la barra de progreso.
-			  try {
-			  Thread.sleep(1000);
-			  } catch (InterruptedException e) { }
-			  num += 10;
-			  }
-	}
 	
 	public void jugarButtonActionPerformed(ActionEvent evt){
 		
-		iterate();
-		RawMessage rawMessageLogin = new RawMessage();
-		rawMessageLogin.type = "personajeConsulta";
-		rawMessageLogin.message = new PersonajeConsultaMessage(new Gson().toJson(usuario));
+		if(!timer.isRunning()){
+			timer.start();
+		}
+		while(!timer.isRunning() && progressBar.getValue() < 100){
+			RawMessage rawMessageLogin = new RawMessage();
+			rawMessageLogin.type = "personajeConsulta";
+			rawMessageLogin.message = new PersonajeConsultaMessage(new Gson().toJson(usuario));
 
-		clienteJugable.getClienteManager().sendMensaje(new Gson().toJson(rawMessageLogin));
-		
-		
-		this.setVisible(false);
-		
+			clienteJugable.getClienteManager().sendMensaje(new Gson().toJson(rawMessageLogin));
+			this.setVisible(false);
+		}
 		
 	}
 }
